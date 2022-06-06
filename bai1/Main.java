3f3a4e916068dc9cf416d602aa1bb8dadaf5cdf5
huuquy222 <huuquynguyen274@gmail.com>
2022-06-06 15:15:37 +0700

    baitapjava

---
+++ bai1/Main.java
@@ -0,0 +1,18 @@
+package bai1;
+
+public class Main {
+    public static void main(String[] args) {
+        int i =10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "ha noi";
+        System.out.println("i= " + i);
+        System.out.println("f= " + i);
+        System.out.println("d= " + i);
+        System.out.println("b= " + i);
+        System.out.println("c= " + i);
+        System.out.println("s= " + i);
+    }
+}
